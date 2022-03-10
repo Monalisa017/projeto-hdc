c068e5c197cd34a6235e799edcd1bfc7d9ef3b80
Monalisa017 <61711279+Monalisa017@users.noreply.github.com>
2022-03-09 22:52:37 -0300

    Iniciando mais um curso de React. Agora vai Queridinha <3 rss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld'
+import Frase from './components/Frase';
 
 function App() {
+
+  const name = 'Monalisa'
+
+  const newName = name.toUpperCase()
+
+  const url = 'https://i.pinimg.com/236x/7a/a9/bd/7aa9bdb576fbbb41a48cb6bbd48c84bf.jpg'
+
+  function soma (a, b){
+    return a + b
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React</h1>
+      <h2>Alterando o JSX</h2>
+      <h3>Olá {name} </h3>
+      <h3> Olá { newName} </h3>
+      <h3> Olá { newName} </h3>
+      <h3>Soma = { soma(5, 5) }</h3>
+      <div>
+      <img src={url} className="App-logo" alt="logo" />
+      <HelloWorld />
+      <Frase />
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/Frase.js
@@ -0,0 +1,8 @@
+function Frase(){
+    return(
+        <div>
+            <p>Este é um componente com um uma frase!</p>
+        </div>
+    )
+}
+export default Frase
